8b3c427a9b45dbc6cc1419470fce5c1bf3d8b7fc
Adil Sikandar <adil.sikandar@mobilelive.ca>
2022-02-11 19:07:38 +0500

    added procfile

--- app/routes.js
+++ app/routes.js
@@ -8,12 +8,8 @@ const routes = new Router();
 // Users
 routes.post('/api/users/register', UsersController.create);
 routes.post('/api/users/login', UsersController.login);
-routes.post('/api/users/resetPassword', UsersController.resetPassword);
 routes.post('/api/users/sendforgetPasswordEmail', UsersController.sendForgetPassEmail);
-routes.post('/api/users/forgetPassword/:userId/:token', UsersController.forgetPassword);
 routes.put('/api/users/updatePassword', UsersController.update);
-routes.get('/api/users/user/:id', UsersController.profileUser);
-
 
 
 routes.use(errorHandler);
